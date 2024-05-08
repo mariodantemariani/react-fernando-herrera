aaaf957bc18e747c5dc41ea76c7ea09702fed057
Mario Dante Mariani <mariodante@gmail.com>
2024-05-08 10:57:51 -0300

    pre

---
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPokemons } from "./store/slices/pokemon/thunks";
+
+export const PokemonApp = () => {
+  const dispatch = useDispatch();
+
+  const { pokemons, isLoading, page } = useSelector((state) => state.pokemons);
+
+  useEffect(() => {
+    dispatch(getPokemons());
+  });
+
+  return (
+    <>
+      <h1>PokemonApp</h1>
+      <hr />
+      {isLoading}
+      <span className="fs-3">Loading: {isLoading ? "True" : "False"}</span>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.name}>{pokemon.name}</li>
+        ))}
+        {/* <li>Pikachu</li>
+        <li>Bulbasaur</li>
+        <li>Squirtle</li> */}
+      </ul>
+      <button disabled={isLoading} onClick={() => dispatch(getPokemons(page))}>
+        Next
+      </button>
+    </>
+  );
+};
